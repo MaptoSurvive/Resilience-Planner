8d307be7cf3e25e7366c8c5d1327adb235e5c942
MaptoSurvive <maptosurvive@proton.me>
2025-08-04 15:54:38 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Resilience Planner</h1>
+      <p>This is the foundation of your state-specific planning tool.</p>
+    </div>
+  )
+}
+add app.jsx
