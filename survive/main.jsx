16932d2e42098be6a5741ae4888f7895b25749cd
MaptoSurvive <maptosurvive@proton.me>
2025-08-04 14:23:34 -0400

    Delete survive directory

--- survive/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ResiliencePlanner from './components/ResiliencePlanner'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ResiliencePlanner />
-  </React.StrictMode>,
-)
