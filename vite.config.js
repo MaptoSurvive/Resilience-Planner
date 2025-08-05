526fc24f9a1f4225370d51023145c2cf8a21f69a
MaptoSurvive <maptosurvive@proton.me>
2025-08-04 20:15:51 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()],
-  base: '/Resilience-Planner/', // Match your repo name
-});
+  build: {
+    outDir: 'docs'  // GitHub Pages auto-detects this
+  },
+  base: '/Resilience-Planner/'  // Match your repo name
+})
